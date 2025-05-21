6ee032ae653ea494577552af108293c4bc7459b7
Douglas Schneider <douglassch09@gmail.com>
2025-05-21 18:05:16 -0300

    Ajuste no relacionamento restricao-restaurante, restaurante-imagem

--- api/projeto/src/main/java/projeto/model/Restaurante.java
+++ api/projeto/src/main/java/projeto/model/Restaurante.java
@@ -4,8 +4,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.Data;
-
+import java.util.List;
 
 @Data
 @Entity
@@ -15,6 +18,16 @@ public class Restaurante {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-     private List<Imagem> imagens;       //erro - esperando classe imagem
-     private List<Restricao> restricoes;  //erro - esperando classe resrticoes
+    @ManyToMany
+    @JoinColumn(name = "id_imagem")
+    private String imagem;    
+    
+    @ManyToMany
+    @JoinTable(
+            name = "restaurante_restricao",
+            joinColumns = @JoinColumn(name = "id_restaurante"),
+            inverseJoinColumns = @JoinColumn(name = "id_restricao")
+    )
+    private List<Restricao> restricoes;
+
 }
