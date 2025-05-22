e808d4dba66c6d8a400a08d69ac4bd7d27c17ae5
DouglasSchneiderdaSilva <100230519+DouglasSchneiderdaSilva@users.noreply.github.com>
2025-05-22 15:40:19 -0300

    ajuste nos atributos model restaurante e relacionamento com endereço

--- api/projeto/src/main/java/projeto/model/Restaurante.java
+++ api/projeto/src/main/java/projeto/model/Restaurante.java
@@ -14,19 +14,31 @@ import java.util.List;
 @Entity
 public class Restaurante {
 
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+    private Long id;
 
-    @ManyToMany
-    @JoinColumn(name = "id_imagem")
-    private String imagem;    
+    private String nome;
+
+    private String email;
+
+    private String senha;
+
+    
+    private String imagem;
+
+    
+    @OneToOne
+    @JoinColumn(name = "id_endereco")
+    private Endereco endereco;
+
     
     @ManyToMany
     @JoinTable(
-            name = "restaurante_restricao",
-            joinColumns = @JoinColumn(name = "id_restaurante"),
-            inverseJoinColumns = @JoinColumn(name = "id_restricao")
+        name = "restaurante_restricao",
+        joinColumns = @JoinColumn(name = "id_restaurante"),
+        inverseJoinColumns = @JoinColumn(name = "id_restricao")
     )
     private List<Restricao> restricoes;
 
