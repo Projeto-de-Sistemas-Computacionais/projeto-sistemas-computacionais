48548f9fa84dd2de65bc75dae08e41cae00fd919
Luisa Romanha <romanhaluisa@gmail.com>
2025-06-12 19:33:49 -0300

    feat: Adicionando a tela de carregamento

---
+++ app/app-restricao/components/telas/TelaCarregamento.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SplashScreen: React.FC = () => {
+  return (
+    <div className="w-screen h-screen bg-gradient-to-b from-[#275B73] to-[#9ED5C1] flex items-center justify-center">
+      <span className="text-white text-sm">LOGO</span>
+    </div>
+  );
+};
+
+export default SplashScreen;
