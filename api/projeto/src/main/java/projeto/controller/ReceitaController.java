d0c80f2a743447b41916a764e57c22d29760dba2
MaRcOs-GaBrIeL15 <marcosgabrielxavieroliveira@gmail.com>
2025-07-03 14:43:24 -0300

    adicionado o filtro na controller com seus devidos caminhos

--- api/projeto/src/main/java/projeto/controller/ReceitaController.java
+++ api/projeto/src/main/java/projeto/controller/ReceitaController.java
@@ -34,6 +34,24 @@ public class ReceitaController {
         return ResponseEntity.ok(response);
     }
 
+    @GetMapping("/titulo/{titulo}")
+    public ResponseEntity<Receita> buscarPorId(@PathVariable String titulo){
+        List<Receita> response = receitaService.buscarPorTitulo(titulo);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/ingredientes/{ingredientes}")
+    public ResponseEntity<Receita> buscarPorId(@PathVariable List<Tipo> ingredientes){
+        List<Receita> response = receitaService.buscarPorIngredientes(ingredientes);
+        return ResponseEntity.ok(response);
+    }
+
+    @GetMapping("/criador/{usuario}")
+    public ResponseEntity<Receita> buscarPorId(@PathVariable Usuario usuario){
+        List<Receita> response = receitaService.buscarPorCriador(usuario);
+        return ResponseEntity.ok(response);
+    }
+
     @PutMapping("{id}")
     public ResponseEntity<Receita> atualizar(@PathVariable Long id, @RequestBody Receita receita){
         Receita response = receitaService.atualizar(id, receita);
