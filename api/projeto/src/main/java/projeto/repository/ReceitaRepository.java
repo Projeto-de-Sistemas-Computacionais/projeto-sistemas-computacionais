0d346933c397560ed4231a8dc5b42b4d3290403f
MaRcOs-GaBrIeL15 <marcosgabrielxavieroliveira@gmail.com>
2025-07-03 14:38:32 -0300

    adicionando os filtros no repository de titulo da receita, ingredientes caso tenha pelo menos um na receita e pelo criador da receita

--- api/projeto/src/main/java/projeto/repository/ReceitaRepository.java
+++ api/projeto/src/main/java/projeto/repository/ReceitaRepository.java
@@ -4,4 +4,13 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import projeto.model.Receita;
 
 public interface ReceitaRepository  extends JpaRepository<Receita, Long> {
+
+    // titulo
+    List<Receita> findByTituloContainingIgnoreCase(String titulo);
+
+    // ingrediente
+    List<Receita> findByIngredientesIn(List<Tipo> ingredientes);
+
+    // criador
+    List<Receita> findByUsuarioCriador(Usuario usuario);
 }
